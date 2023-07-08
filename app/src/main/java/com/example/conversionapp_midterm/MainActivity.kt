05011736d93dea19c1a5dcd9f57f27983baaba34
Temitope Adebiyi <adebiyitope26@gmail.com>
2023-07-08 18:33:05 +0100

    added makeConversion function

--- app/src/main/java/com/example/conversionapp_midterm/MainActivity.kt
+++ app/src/main/java/com/example/conversionapp_midterm/MainActivity.kt
@@ -31,5 +31,30 @@ class MainActivity : AppCompatActivity() {
 
         // Set the adapter for the Spinner
         binding.converUnitSpinner.adapter = adapter
+
+        // Set a click listener for the Calculate button
+        binding.calculateButton.setOnClickListener {
+            // Get the input value from the EditText
+            val inputValue = binding.inputEditText.text.toString().toDoubleOrNull()
+
+            // Perform the conversion based on the selected item in the Spinner
+            val selectedItem = binding.converUnitSpinner.selectedItem.toString()
+            val result = makeConversion(inputValue, selectedItem)
+
+            // Update the output TextView with the result
+            binding.outputTextView.text = result?.toString() ?: "Invalid input"
+        }
+    }
+
+    private fun makeConversion(input: Double?, conversionUnit: String): Double? {
+        return when (conversionUnit.lowercase()) {
+            "km to mi" -> input?.times(0.62)
+            "mi to km" -> input?.times(1.61)
+            "cm to km" -> input?.div(100000)
+            "in to cm" -> input?.times(2.54)
+            "kg to lb" -> input?.times(2.2)
+            "lb to kg" -> input?.times(0.45)
+            else -> null
+        }
     }
 }
